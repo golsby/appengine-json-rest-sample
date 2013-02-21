b36a58edb7193de61d1e46d4ce760fae8b4656b1
golsby <brian@mcneel.com>
2013-02-20 16:10:29 -0800

    First working AppEngine App.

---
+++ main.py
@@ -0,0 +1,18 @@
+import packages # required for AppEngine to find third-party packages.
+from google.appengine.ext import db
+
+from appengine_json_rest.appengine_json_rest import create_application
+
+
+class Fruit(db.Model):
+    name = db.StringProperty()
+    width = db.IntegerProperty()
+    created_datetime = db.DateTimeProperty(required=True, auto_now_add=True)
+    modified_datetime = db.DateTimeProperty(required=True, auto_now=True, auto_now_add=True)
+    created_date = db.DateProperty(required=True, auto_now_add=True)
+    modified_date = db.DateProperty(required=True, auto_now=True, auto_now_add=True)
+    created_time = db.TimeProperty(required=True, auto_now_add=True)
+    modified_time = db.TimeProperty(required=True, auto_now=True, auto_now_add=True)
+
+
+thingy = create_application('rest', models = [Fruit])
---
+++ packages/__init__.py
@@ -0,0 +1,5 @@
+__author__ = 'Brian'
+import sys
+package_dir = 'packages'
+sys.path.insert(0, package_dir)
+

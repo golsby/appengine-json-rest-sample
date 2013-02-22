fb18e985faabe42a2483577065dfc199054441c7
golsby <brian@mcneel.com>
2013-02-22 09:56:46 -0800

    Started writing some tests.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import packages # required for AppEngine to find third-party packages.
 from google.appengine.ext import db
+import datetime
 
 from appengine_json_rest.appengine_json_rest import create_application
 
@@ -13,6 +14,11 @@ class Fruit(db.Model):
     modified_date = db.DateProperty(required=True, auto_now=True, auto_now_add=True)
     created_time = db.TimeProperty(required=True, auto_now_add=True)
     modified_time = db.TimeProperty(required=True, auto_now=True, auto_now_add=True)
+    rating = db.RatingProperty()
+    location = db.GeoPtProperty()
+    aliases = db.StringListProperty()
+    destinations = db.ListProperty(db.GeoPt)
+    touched_dates = db.ListProperty(datetime.datetime)
 
 
 simple = create_application('simple', models = [Fruit])
